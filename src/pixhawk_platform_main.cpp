d17785cb483381049f787029323417e8286309d6
Miguel <miferco97@gmail.com>
2021-12-01 16:34:39 +0100

    new as2::spinLoop() implemented

--- src/pixhawk_platform_main.cpp
+++ src/pixhawk_platform_main.cpp
@@ -1,12 +1,16 @@
 // "Copyright [year] <Copyright Owner>"
 
 #include "pixhawk_platform.hpp"
+#include "as2_core/core_functions.hpp"
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = std::make_shared<PixhawkPlatform>();
-  node->spinLoop(300);
+  node->preset_loop_frequency(300);
+  as2::spinLoop(node);  
+  
+
   rclcpp::shutdown();
   return 0;
 }

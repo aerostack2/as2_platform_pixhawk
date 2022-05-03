f8d079c5895b138cec091c57b09f2182eca3e33f
Pedro Arias <pedroariasperez96@gmail.com>
2022-05-03 13:05:39 +0200

    Added pixhawk control modes to yaml

--- launch/pixhawk_platform_launch.py
+++ launch/pixhawk_platform_launch.py
@@ -1,17 +1,25 @@
+from os.path import join
+
 import launch
 from launch import LaunchDescription, conditions
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, EnvironmentVariable
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+    config = join(
+        get_package_share_directory('pixhawk_platform'),
+        'config',
+        'control_modes.yaml'
+    )
     return LaunchDescription([
         DeclareLaunchArgument('drone_id', default_value='drone0'),
         DeclareLaunchArgument('mass', default_value='1.0'),
         DeclareLaunchArgument('simulation_mode', default_value='false'),
         DeclareLaunchArgument('max_thrust', default_value='0.0'),
-        DeclareLaunchArgument('control_modes_file', default_value='config/control_modes.yaml'),
+        DeclareLaunchArgument('control_modes_file', default_value=config),
         # if is not in simulation
         Node(
             package="pixhawk_platform",
